cdf530751ac69289e39133d36cf374d7a1d2042e
Vanderlei I Martins <vanderlei.i.martins@outlook.com>
2021-02-04 14:30:12 +0000

    Completed lab.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+//const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,7 +21,38 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    /* Iteration 2 - INSERTING ONE RECIPE */
+    Recipe.create({title: 'Rice', level: 'Easy Peasy', ingredients: ['rice', 'water'], cuisine: 'casual', dishType: 'main_course', duration: 1, creator: 'someone'})
+    .then((createdRecipe) => {
+      console.log(`You created the fantastic recipe of ${createdRecipe.title}`);
+
+        /* Iteration 3 - INSERTING RECIPES FROM DATA.JSON*/
+        /* after doing this I saw the data variable ¬¬ */
+        const fs = require('fs');
+        let content = JSON.parse(fs.readFileSync('data.json'));
+        Recipe.insertMany(content)
+        .then(() => {
+          console.log(`Recipes inserted`);
+
+          const updateDeleteAndClose = async () => {
+            try {
+              /*Iteration 4 - UPDATING A RECIPE*/
+              await Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration: 100});
+              console.log('Recipe Updated!');
+
+              /*Iteration 5 - REMOVING A RECIPE*/
+              await Recipe.deleteOne({title: 'Carrot Cake'});
+              console.log('Recipe deleted');
+
+              /*Iteration 6 - CLOSING THE CONNECTION*/
+              mongoose.connection.close();
+            } catch (error) {
+              console.log(error);
+            }
+          };
+          updateDeleteAndClose();
+        });
+    });
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
